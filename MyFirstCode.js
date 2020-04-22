ee517b26cc9069c8a3e27fc423354e705910c979
Stefano <bruzzese.stefano@gmail.com>
2020-04-22 11:55:05 +0200

    w3d2

---
+++ MyFirstCode.js
@@ -0,0 +1,64 @@
+function testMe() {
+    let test1 = 5;
+    let test2 = 6;
+    let test3 = 7;
+    let result = test1*test2*test3;
+    return result;
+}
+
+function AI(x) {
+    if (x == 'hello') {
+        return 'hi';
+    }
+    else {
+        return 'I cannot understand';
+    }
+}
+
+function tripleSum(x, y, z) {
+    var a = x * y* z;
+    return a;
+}
+
+function factorial(n)
+{ 
+  var i,f 
+  f=1
+  for(i=n;i>1;i--)
+  {
+     f=f*i
+  }
+  return(f)
+}
+
+function calculator()
+
+function absolute (n) {
+    return Math.abs(n);
+}
+
+function fizzBuzz (x){
+if (x % 21 == 0) {
+    return 'fizzbuzz';
+  }
+  else if (x % 7 == 0) {
+    return 'buzz';
+  }
+  else if (x % 3 == 0)  {
+    return 'fizz';
+  }else{
+    return x;
+  }
+}
+
+function positive (x) {
+    if (x > 0) {
+    return true;
+}
+else {
+    return false;
+}
+}
+
+function transform ()
+
